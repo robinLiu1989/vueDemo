06f3545bcae925fe10928e75bcbca10f67a0336d
liuchun <robin_liu1989@sina.com>
2017-09-13 10:26:27 +0800

    firstcommit

---
+++ myproject/src/components/Hello.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="example-2">
+    <button @click="show = !show">Toggle show</button>
+    <transition name="bounce">
+      <p v-if="show">Look at me!</p>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      show: true
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+  .bounce-enter-active {
+    animation: bounce-in .5s;
+  }
+  .bounce-leave-active {
+    animation: bounce-in .5s reverse;
+  }
+  @keyframes bounce-in {
+    0% {
+      transform: translateX(0) rotate(0);
+
+    }
+    50% {
+      transform: translateX(100px) rotate(360deg);
+    }
+    100% {
+      transform: translateX(0)  rotate(0);
+    }
+  }
+
+
+</style>
